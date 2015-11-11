93eb9cddeda5215eb1ab1c27a1df32f59525ecd7
Niklas Jonsson <niklasandersjonsson@gmail.com>
2015-11-11 20:32:42 +0100

    Shortest done except for file parsing and syso printing

--- learn_you_a_haskell/shortest.hs
+++ learn_you_a_haskell/shortest.hs
@@ -5,8 +5,20 @@ data Label = A | B | C deriving (Show)
 type Path = [(Label, Int)]
 
 optimalPath :: RoadSystem -> Path
-optimalPath [x] = 
-optimalPath x:xs = 
+optimalPath roadSystem =
+	let (bestA, bestB) = foldl roadStep ([],[]) roadSystem
+	in if sum (map snd bestA) <= sum (map snd bestB)
+		then reverse bestA
+		else reverse bestB
 
 roadStep :: (Path, Path) -> Section -> (Path, Path)
-roadStep (toA, ToB) s = (newToA, newToB	
+roadStep (toA, toB) s = (newToA, newToB)
+	where 	
+		pA = sum $ map snd toA
+		pB = sum $ map snd toB
+		aA = getA s + pA
+		bA = getB s + getC s + pB
+		bB = getB s + pB
+		aB = getA s + getC s + pA
+		newToA = if aA < bA then (A, getA s):toA else (C, getC s):(B, getB s):toB
+		newToB = if bB < aB then (B, getB s):toB else (C, getC s):(A, getA s):toA
